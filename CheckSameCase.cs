d1b669ea72c817d55766483ed4e30f69f3f84bc9
Ryan Buchanan <buchanan.ryan22@gmail.com>
2022-05-19 11:53:45 -0600

    Add project files.

---
+++ CheckSameCase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CheckSameCase
+{
+    class CheckSameCase
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
